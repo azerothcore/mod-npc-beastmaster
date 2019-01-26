c8c9906c6b9d04a6b3ae9a737d05c7ae0f0b6a8e
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-01-26 00:08:34 +0000

    Feat(DB/SAI): Remove loading config from cpp

--- src/npc_beastmaster.cpp
+++ src/npc_beastmaster.cpp
@@ -356,17 +356,6 @@ public:
     void OnBeforeConfigLoad(bool reload) override
     {
         if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path+"/npc_beastmaster.conf";
-#ifdef WIN32
-            cfg_file = "npc_beastmaster.conf";
-#endif
-            std::string cfg_def_file = cfg_file + ".dist";
-
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-
-            sConfigMgr->LoadMore(cfg_file.c_str());
-
             BeastMasterAnnounceToPlayer = sConfigMgr->GetBoolDefault("BeastMaster.Announce", true);
             BeastMasterHunterOnly = sConfigMgr->GetBoolDefault("BeastMaster.HunterOnly", true);
             BeastMasterAllowExotic = sConfigMgr->GetBoolDefault("BeastMaster.AllowExotic", true);
